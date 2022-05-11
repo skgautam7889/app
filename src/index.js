d2322fbabbd1984b93f8e68e3550a9354362ae42
Suraj Kumar <surajkumargautam7889@gmail.com>
2022-05-11 09:04:19 +0530

    add sub pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <>
+    <div className="cards">
+      <div className="card">
+        <img src="https://wallpaperaccess.com/full/2081850.jpg" height="150" width="150"/ >
+        <div className="card_info">
+          <span className="card_category"> PHP</span>
+          <h3 className="card_title"></h3>
+          <a href="#" target="_blank" className="">
+            <button> Watch Now</button>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className="cards">
+      <div className="card">
+        <img src="https://wallpaperaccess.com/full/2081850.jpg" height="150" width="150"/ >
+        <div className="card_info">
+          <span className="card_category"> PHP</span>
+          <h3 className="card_title"></h3>
+          <a href="#" target="_blank" className="">
+            <button> Watch Now</button>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className="cards">
+      <div className="card">
+        <img src="https://wallpaperaccess.com/full/2081850.jpg" height="150" width="150"/ >
+        <div className="card_info">
+          <span className="card_category"> PHP</span>
+          <h3 className="card_title"></h3>
+          <a href="#" target="_blank" className="">
+            <button> Watch Now</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  </>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
